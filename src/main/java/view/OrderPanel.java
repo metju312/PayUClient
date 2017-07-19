164cf4dafbf7db8de5ffb8507cf775e48bdd650e
Matthew <metju312@gmail.com>
2017-07-19 20:51:50 +0200

    add orderPanel

---
+++ src/main/java/view/OrderPanel.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class OrderPanel extends JPanel {
+    private JFrame mainWindow;
+
+    public OrderPanel(JFrame mainWindow) {
+        this.mainWindow = mainWindow;
+    }
+}
